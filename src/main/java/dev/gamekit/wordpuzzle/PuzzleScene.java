abb206fa63c6cf089ac78b770d211830c89ab3bb
Kwame Opare Asiedu <kwame@gamekit.dev>
2026-03-21 23:23:03 +0000

    Implemented a timer with custom font which stops when all words are found

--- src/main/java/dev/gamekit/wordpuzzle/PuzzleScene.java
+++ src/main/java/dev/gamekit/wordpuzzle/PuzzleScene.java
@@ -1,5 +1,7 @@
 package dev.gamekit.wordpuzzle;
 
+import dev.gamekit.core.Application;
+import dev.gamekit.core.IO;
 import dev.gamekit.core.Renderer;
 import dev.gamekit.core.Scene;
 import dev.gamekit.ui.enums.Alignment;
@@ -16,10 +18,12 @@ import java.util.Arrays;
 
 public class PuzzleScene extends Scene {
   private static final String[] WORDS = new String[]{ "OSCARS", "AYRA", "OMAH", "OMOTOLA", "AFROBEATS" };
+  private static final Font CLOCK_FONT = IO.getFont("digital-7-mono.ttf");
 
   private final Puzzle puzzle;
   private final java.util.List<String> foundWords;
   private final java.util.List<Slot> validSlots;
+  private long timerMs = 0;
 
   public PuzzleScene() {
     super("Puzzle Scene");
@@ -27,6 +31,18 @@ public class PuzzleScene extends Scene {
     puzzle = new Puzzle(12, 12, WORDS);
     foundWords = new ArrayList<>();
     validSlots = new ArrayList<>();
+  }
+
+  @Override
+  protected void update() {
+    if (foundWords.size() < WORDS.length) {
+      long newTimerMs = timerMs + Application.FRAME_INTERVAL_MS;
+
+      if (newTimerMs / 1000 > timerMs / 1000)
+        updateUI();
+
+      timerMs = newTimerMs;
+    }
   }
 
   @Override
@@ -45,7 +61,8 @@ public class PuzzleScene extends Scene {
         Padding.create(
           96,
           Column.create(
-            props -> props.crossAxisAlignment = CrossAxisAlignment.CENTER, Text.create(
+            props -> props.crossAxisAlignment = CrossAxisAlignment.CENTER,
+            Text.create(
               props -> {
                 props.text = "Find These Words";
                 props.fontSize = 32;
@@ -77,35 +94,66 @@ public class PuzzleScene extends Scene {
           )
         )
       ),
-      Center.create(
-        Sized.create(
-          props -> {
-            props.fixedWidth = 768.0;
-            props.fixedHeight = 768.0;
-          },
-          GestureDetector.create(
-            (GestureDetector.GestureDetectorConfig.Updater) props -> {
-              props.puzzle = puzzle;
-              props.validSlots = validSlots;
-              props.onSlotMarked = this::onSlotMarked;
-            },
-            Grid.create(
+      Align.create(
+        props -> {
+          props.horizontalAlignment = Alignment.CENTER;
+          props.verticalAlignment = Alignment.START;
+        },
+        Padding.create(
+          48,
+          Stack.create(
+            Opacity.create(
+              0.25,
+              Text.create(
+                props -> {
+                  props.text = "00:00";
+                  props.font = CLOCK_FONT;
+                  props.fontSize = 96;
+                }
+              )
+            ),
+            Text.create(
               props -> {
-                props.columnCount = puzzle.cols;
-                props.columnGapSize = props.rowGapSize = 0;
+                props.text = formatTimer();
+                props.font = CLOCK_FONT;
+                props.fontSize = 96;
+              }
+            )
+          )
+        )
+      ),
+      Padding.create(
+        128, 0, 0, 0,
+        Center.create(
+          Sized.create(
+            props -> {
+              props.fixedWidth = 768.0;
+              props.fixedHeight = 768.0;
+            },
+            GestureDetector.create(
+              (GestureDetector.GestureDetectorConfig.Updater) props -> {
+                props.puzzle = puzzle;
+                props.validSlots = validSlots;
+                props.onSlotMarked = this::onSlotMarked;
               },
-              Arrays.stream(puzzle.chars).map(
-                (ch) -> Center.create(
-                  Text.create(
-                    props -> {
-                      props.text = ch;
-                      props.fontStyle = Text.BOLD;
-                      props.fontSize = 36;
-                      props.fontHeightRatio = 0.8;
-                    }
+              Grid.create(
+                props -> {
+                  props.columnCount = puzzle.cols;
+                  props.columnGapSize = props.rowGapSize = 0;
+                },
+                Arrays.stream(puzzle.chars).map(
+                  (ch) -> Center.create(
+                    Text.create(
+                      props -> {
+                        props.text = ch;
+                        props.fontStyle = Text.BOLD;
+                        props.fontSize = 36;
+                        props.fontHeightRatio = 0.8;
+                      }
+                    )
                   )
-                )
-              ).toArray(Widget[]::new)
+                ).toArray(Widget[]::new)
+              )
             )
           )
         )
@@ -113,7 +161,7 @@ public class PuzzleScene extends Scene {
     );
   }
 
-  void onSlotMarked(Slot slot) {
+  private void onSlotMarked(Slot slot) {
     String slotWord = puzzle.getSlotWord(slot);
 
     if (Arrays.asList(WORDS).contains(slotWord) && !foundWords.contains(slotWord)) {
@@ -122,5 +170,12 @@ public class PuzzleScene extends Scene {
       validSlots.add(slot);
       updateUI();
     }
+  }
+
+  private String formatTimer() {
+    long timerSecs = timerMs / 1000;
+    long secs = timerSecs % 60;
+    long mins = timerSecs / 60;
+    return String.format("%02d:%02d", mins, secs);
   }
 }
